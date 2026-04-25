3c1727a65c2ebeebc4245ca15ba81f07480c5070
Esma Nur <esmanurkokoren@gmail.com>
2026-04-26 00:28:07 +0300

    feat: main

---
+++ Speculation/Assets/Scripts/Main.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public GameObject PlayerPref;
+    private void Awake()
+    {
+        SpawnIt();
+    }
+
+
+    private void SpawnIt()
+    {
+        Instantiate(PlayerPref, spawnPoint.position, spawnPoint.rotation);
+    }
+}
